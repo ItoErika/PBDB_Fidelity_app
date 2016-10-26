5c920b7d2dc2e30e024b6cb6cdd1e211d6ddb31b
Erika Ito <etito@wisc.edu>
2016-10-26 14:31:03 -0500

    Update Unit_Matrix.R

--- Development/Unit_Matrix.R
+++ Development/Unit_Matrix.R
@@ -23,7 +23,7 @@ LithMatrix<-sapply(Lithologies,function(x,y) grepl(x,y,ignore.case=FALSE, perl =
 LithMatrix[,1:ncol(LithMatrix)]<-as.numeric(LithMatrix[,1:ncol(LithMatrix)])
 
 # Bind the LithMatrix to the "strat_name_long" column of SubsetUnitsFrame
-LithMatrix<-as.data.frame(cbind(as.character(SubsetUnitsFrame[,"strat_name_long"]),LithMatrix))
+UnitDataTable<-as.data.frame(cbind(as.character(SubsetUnitsFrame[,"strat_name_long"]),LithMatrix))
 
 # Create a vector of all the state/location names
 # Connet to PostgreSQL
@@ -37,9 +37,23 @@ GroupedLocationsList<-tapply(LocationTuples[,"location"],LocationTuples[,"col_id
 # Collapse the elements for each "col_id" in the list so that a vector is returned
 GroupedLocations<-sapply(GroupedLocations, function(x) paste(x, collapse=' '))
 # make a vector of each respective col_id for the collapsed lcoations
-ColID<-as.numeric(names(GroupedLocationsVector))
+col_id<-as.numeric(names(GroupedLocationsVector))
 # bind the ColID vector with the GroupedLocations vector
-ColIDLocationData<-as.data.frame(cbind(ColID,GroupedLocations))
+ColIDLocationData<-as.data.frame(cbind(col_id,GroupedLocations))
 # Convert the GroupedLocations column to character data
 ColIDLocationData[,"GroupedLocations"]<-as.character(ColIDLocationData[,"GroupedLocations"])
   
+# merge the GroupedLocationsData to SubsetUnitsFrame by col_id
+SubsetUnitsFrame<-merge(SubsetUnitsFrame,ColIDLocationData,by="col_id", all.x=TRUE)
+  
+# Create a vector of locations
+Locations<-unique(LocationTuples[,"location"])
+# Remove blanks
+Locations<-na.omit(Locations)
+# Create a matrix showing whether or not each location corresponds with each row or SubsetUnitsFrame[,"GroupedLocations"]
+LocationMatrix<-sapply(Locations,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),as.character(SubsetUnitsFrame[,"GroupedLocations"]))
+# Convert the logical data into numerical data
+LocationMatrix[,1:ncol(LocationMatrix)]<-as.numeric(LocationMatrix[,1:ncol(LocationMatrix)])
+  
+# Bind the LocationMatrix to LithMatrix
+UnitDataTable<-as.data.frame(cbind(LithMatrix,LocationMatrix))
