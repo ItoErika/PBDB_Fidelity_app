3f102470ae9775895aa201add4228f311ba0b12d
Erika Ito <etito@wisc.edu>
2016-11-02 14:04:44 -0500

    Update and rename Locations.R to Location_Relationships.R

--- Development/Location_Relationships.R
+++ Development/Location_Relationships.R
@@ -1,3 +1,5 @@
+# Test the relationship between the number of PBDB occurrences and the GDP and population of U.S. states.
+
 library("RCurl")
 
 options(timeout=600)
@@ -7,3 +9,9 @@ FossilsFrame<-read.csv(text=GotURL,header=TRUE)
 
 # Create a matrix of the number of PBDB occurrences per state
 StateOccurrences<-as.matrix(table(FossilsFrame[,"state"]))
+
+# Load U.S. state GDP and population data
+StatePopGDP<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/state2015_pop_gdp.csv")
+colnames(StatePopGDP)[1]<-"Pop2015"
+colnames(StatePopGDP)[2]<-"GDP2015"
+
