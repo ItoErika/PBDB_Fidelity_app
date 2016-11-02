0c819572c06e4ab287f865d25ff48a7fcad51a4d
Erika Ito <etito@wisc.edu>
2016-11-02 14:01:23 -0500

    Update Locations.R

--- Development/Locations.R
+++ Development/Locations.R
@@ -1,1 +1,9 @@
+library("RCurl")
 
+options(timeout=600)
+FossilURL<-"https://paleobiodb.org/data1.2/occs/list.csv?base_name=eukaryota&show=loc,strat,lith,lithext,geo"
+GotURL<-getURL(FossilURL)
+FossilsFrame<-read.csv(text=GotURL,header=TRUE)
+
+# Create a matrix of the number of PBDB occurrences per state
+StateOccurrences<-as.matrix(table(FossilsFrame[,"state"]))
