6491f7721653a04b6a458bcf8a9e2b81ee43d3b1
Erika Ito <etito@wisc.edu>
2016-11-04 16:26:50 -0500

    Update Output_Refining.R

--- Output_Refining.R
+++ Output_Refining.R
@@ -17,13 +17,18 @@ Below<-grep("below", OutputData[,"Sentence"], ignore.case=TRUE, perl=TRUE)
 UnwantedRows<-unique(c(Micro,Trace,NoFossils,LackFossils,LackOfFossils,AbsentFossils,VoidFossils,Correlative,Equivalent,Above,Below))
                          
 CleanedOutput<-OutputData[-UnwantedRows,]
+
+
+
+Make a sample table of unique unit name matches and sentences
+Sample<-unique(CleanedOutput[,c("strat_name_long","Sentence")])
                          
-# Take a random sample of 100 Stage1Output Rows to check accuracy
-# CleanedSampleOutput2<-CleanedOutput[sample(c(1:nrow(CleanedOutput)),100,replace=FALSE),]
+# Take a random sample of 100  Sample rows to check accuracy
+# CleanedSampleOutput3<-Sample[sample(c(1:nrow(Sample)),100,replace=FALSE),]
 
 
 # Save SampleOutput1 to a folder
-# write.csv(CleanedSampleOutput2,file="~/Documents/DeepDive/PBDB_Fidelity/R/CleanedSampleOutput2.csv",row.names=FALSE)
+# write.csv(CleanedSampleOutput3,file="~/Documents/DeepDive/PBDB_Fidelity/R/CleanedSampleOutput3.csv",row.names=FALSE)
 # Open the csv in excel or libre office and perform a manual accuracy test
 # Renamed "CleanedSampleOutput2_Completed.csv"                   
 
