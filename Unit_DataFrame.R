b9c905d72d4417414bec2cd0050a227770232edb
Erika Ito <etito@wisc.edu>
2016-11-18 11:56:01 -0600

    Update Unit_DataFrame.R

--- Unit_DataFrame.R
+++ Unit_DataFrame.R
@@ -4,32 +4,16 @@ library("RCurl")
 library("RPostgreSQL")
 
 # Download all marine, sedimentary unit names from Macrostrat Database
-UnitsURL<-paste("https://macrostrat.org/api/units?lith_class=sedimentary&project_id=1&response=long&format=csv")
+UnitsURL<-"https://macrostrat.org/api/units?lith_class=sedimentary&project_id=1&response=long&format=csv"
 GotURL<-getURL(UnitsURL)
 UnitsFrame<-read.csv(text=GotURL,header=TRUE)
 
 #Load all of the candidate units (marine, sedimentary, unfossiliferous) that were matched in the documents
-UnitHitData<-readRDS("~/Documents/DeepDive/PBDB_Fidelity/output_11_2_2016/UnitHitData.rds")
+UnitHitData<-readRDS("~/Documents/DeepDive/PBDB_Fidelity/output_11_18_2016/UnitHitData.rds")
 # Extract all unit name matches
 MatrixUnits<-unique(UnitHitData[,"UnitName"])
 # Subset UnitsFrame to only include units from UnitHitData
 SubsetUnitsFrame<-UnitsFrame[which(UnitsFrame[,"strat_name_long"]%in%MatrixUnits),]
-
-############################################# CREATE LITHOLOGY COLUMNS ###################################################
-
-# Create a vector of lithology categories from SubsetUnitsFrame
-Lithologies<-(c("amphibolite","ash","andesite","argillite","arkose","basalt","breccia","chalk","chert","clay","coal","conglomerate",
-"dacite","diamictite","dolomite","gabbro","gneiss","gravel","graywacke","greywacke","evaporite","lignite","limestone","marble",
-"marl","mudstone","oolitic limestone","phosophorite","phyllite","quartzite","rhyolite","sand ","sandstone","schist","shale",
-"siliciclastic","silt ","siltstone","silty clay","silty sand ","skeletal silt","slate","tuff","volcanic"))
-
-# Create a matrix showing whether or not each lithology category corresponds with each row of SubsetUnitsFrame[,"lith"]
-LithMatrix<-sapply(Lithologies,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),SubsetUnitsFrame[,"lith"])
-# Convert the logical data into numerical data
-LithMatrix[,1:ncol(LithMatrix)]<-as.numeric(LithMatrix[,1:ncol(LithMatrix)])
-# Name final matrix
-UnitDataTable<-data.matrix(LithMatrix)
-rownames(UnitDataTable)<-as.character(SubsetUnitsFrame[,"unit_id"])
 	
 ############################################# CREATE LOCATION COLUMNS ####################################################	
 	
@@ -43,46 +27,47 @@ LocationTuples<-dbGetQuery(Connection,"SELECT* FROM column_locations.intersectio
 # Group LocationTuple data by "col_id" column
 GroupedLocationsList<-tapply(LocationTuples[,"location"],LocationTuples[,"col_id"],as.character)
 # Collapse the elements for each "col_id" in the list so that a vector is returned
-GroupedLocations<-sapply(GroupedLocationsList, function(x) paste(x, collapse=' '))
-# make a vector of each respective col_id for the collapsed lcoations
-col_id<-as.numeric(names(GroupedLocations))
-# bind the ColID vector with the GroupedLocations vector
-ColIDLocationData<-as.data.frame(cbind(col_id,GroupedLocations))
+GroupedLocations<-setNames(as.data.frame(sapply(GroupedLocationsList, function(x) paste(x, collapse=' '))),"States")
 # Convert the GroupedLocations column to character data
-ColIDLocationData[,"GroupedLocations"]<-as.character(ColIDLocationData[,"GroupedLocations"])
+GroupedLocations[,"States"]<-as.character(GroupedLocations[,"States"])
   
 # merge the GroupedLocationsData to SubsetUnitsFrame by col_id
-SubsetUnitsFrame<-merge(SubsetUnitsFrame,ColIDLocationData,by="col_id", all.x=TRUE)
+SubsetUnitsFrame<-merge(SubsetUnitsFrame,GroupedLocations,by.x="col_id",by.y="row.names", all.x=TRUE)
   
 # Create a vector of locations
 Locations<-unique(LocationTuples[,"location"])
 # Remove blanks
 Locations<-na.omit(Locations)
 # Create a matrix showing whether or not each location corresponds with each row or SubsetUnitsFrame[,"GroupedLocations"]
-LocationMatrix<-sapply(Locations,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),as.character(SubsetUnitsFrame[,"GroupedLocations"]))
+LocationMatrix<-sapply(Locations,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),as.character(SubsetUnitsFrame[,"States"]))
 # Convert the logical data into numerical data
 LocationMatrix[,1:ncol(LocationMatrix)]<-as.numeric(LocationMatrix[,1:ncol(LocationMatrix)])
   
-# Bind the LocationMatrix to LithMatrix
-UnitDataTable<-data.matrix(cbind(UnitDataTable,LocationMatrix))
+# Name final matrix
+UnitDataTable<-data.matrix(LocationMatrix)
+rownames(UnitDataTable)<-as.character(SubsetUnitsFrame[,"unit_id"])
+	
+############################################# CREATE LITHOLOGY COLUMNS ###################################################
+
+# Create a vector of lithology categories from SubsetUnitsFrame
+Lithologies<-(c("amphibolite","ash","andesite","argillite","arkose","basalt","breccia","chalk","chert","clay","coal","conglomerate",
+"dacite","diamictite","dolomite","gabbro","gneiss","gravel","graywacke","greywacke","evaporite","lignite","limestone","marble",
+"marl","mudstone","oolitic limestone","phosophorite","phyllite","quartzite","rhyolite","sand ","sandstone","schist","shale",
+"siliciclastic","silt ","siltstone","silty clay","silty sand ","skeletal silt","slate","tuff","volcanic"))
+
+# Create a matrix showing whether or not each lithology category corresponds with each row of SubsetUnitsFrame[,"lith"]
+LithMatrix<-sapply(Lithologies,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),SubsetUnitsFrame[,"lith"])
+# Convert the logical data into numerical data
+LithMatrix[,1:ncol(LithMatrix)]<-as.numeric(LithMatrix[,1:ncol(LithMatrix)])
+
+# Bind the LithMatrix to UnitDataTable
+UnitDataTable<-data.matrix(cbind(UnitDataTable,LithMatrix))
 
 ############################################## CREATE TIME COLUMNS ###################################################
 	
 # Load communityMatrix.R modgule of paleobiology database r package
 source("https://raw.githubusercontent.com/aazaff/paleobiologyDatabase.R/master/communityMatrix.R")
 	
-# download timescale data from macrostrat
-function(Timescale) {
-	Timescale<-gsub(" ","%20",Timescale)
-	URL<-paste("https://dev.macrostrat.org/api/defs/intervals?format=csv&timescale=",Timescale,sep="")
-	GotURL<-getURL(URL)
-	Intervals<-read.csv(text=GotURL,header=T)
-	Midpoint<-apply(Intervals[,c("t_age","b_age")],1,median)
-	Intervals<-cbind(Intervals,Midpoint)
-	rownames(Intervals)<-Intervals[,"name"]
-	return(Intervals)
-	}
-  
 Periods<-downloadTime("international%20periods")
 	
 # create a matrix showing whether or not each period from Periods corresponds with each unit of SubsetUnitsFrame  
@@ -117,6 +102,23 @@ for(i in Separated){
 	
 # Bind the AgeMatrix to UnitDataTable
 UnitDataTable<-data.matrix(cbind(UnitDataTable,AgeMatrix))
+
+##################################### ADD ENVIRONMENTS COLUMNS ###########################################
+
+# download a list of environmnets from Macrostrat database
+EnvironsURL<-"https://macrostrat.org/api/defs/environments?all&format=csv"
+GotURL<-getURL(EnvironsURL)
+EnvironsFrame<-read.csv(text=GotURL,header=TRUE)
+Environments<-unique(EnvironsFrame[,"name"])
+	
+# Create a matrix showing whether or not each environment category corresponds with each row of SubsetUnitsFrame[,"environ"]
+EnvironMatrix<-sapply(Environments,function(x,y) grepl(x,y,ignore.case=FALSE, perl = TRUE),SubsetUnitsFrame[,"environ"])
+# assign column names
+colnames(EnvironMatrix)<-Environments
+
+UnitDataTable<-data.matrix(cbind(UnitDataTable,EnvironMatrix))
+	
+# UnitDataTable[which(UnitDataTable[,"non-marine"]=="1"),"marine"]<-0
 	
 ###################### Find which rows in the UnitDataTable were found in the cleaned app output ########################
 
@@ -128,3 +130,4 @@ CleanedOutputUnits<-unique(CleanedOutput[,"strat_name_long"])
 OutputUnitID<-SubsetUnitsFrame[which(SubsetUnitsFrame[,"strat_name_long"]%in%CleanedOutputUnits),"unit_id"]
 # Find which unit_ids associated with units in CleanedOutput are in UnitDataTable
 FossilUnitIDs<-rownames(UnitDataTable[which(rownames(UnitDataTable)%in%OutputUnitID),])
+	
