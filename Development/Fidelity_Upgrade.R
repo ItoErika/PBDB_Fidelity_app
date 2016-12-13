91890e97ea03d09d0e3af412c7324653f127400c
Erika Ito <etito@wisc.edu>
2016-12-13 12:50:58 -0600

    Update Fidelity_Upgrade.R

--- Development/Fidelity_Upgrade.R
+++ Development/Fidelity_Upgrade.R
@@ -1,1 +1,47 @@
+Start<-print(Sys.time())
 
+if (require("doParallel",warn.conflicts=FALSE)==FALSE) {
+    install.packages("doParallel",repos="http://cran.cnr.berkeley.edu/");
+    library("doParallel");
+    }
+
+if (require("RPostgreSQL",warn.conflicts=FALSE)==FALSE) {
+    install.packages("RPostgreSQL",repos="http://cran.cnr.berkeley.edu/");
+    library("RPostgreSQL");
+    }
+
+# Start a cluster for multicore, 4 by default or higher if passed as command line argument
+CommandArgument<-commandArgs(TRUE)
+if (is.na(CommandArgument)) {
+    Cluster<-makeCluster(3)
+    } else {
+    Cluster<-makeCluster(as.numeric(CommandArgument[1]))
+    }
+
+# Download the config file
+Credentials<-as.matrix(read.table("Credentials.yml",row.names=1))
+
+print(paste("Begin loading postgres tables.",Sys.time()))
+
+# Connet to PostgreSQL
+Driver <- dbDriver("PostgreSQL") # Establish database driver
+Connection <- dbConnect(Driver, dbname = Credentials["database:",], host = Credentials["host:",], port = Credentials["port:",], user = Credentials["user:",])
+# STEP ONE: Load DeepDiveData 
+# Make SQL query
+DeepDiveData<-dbGetQuery(Connection,"SELECT docid, sentid, words FROM nlp_sentences_352") 
+
+# If testing: 
+#Driver <- dbDriver("PostgreSQL") # Establish database driver
+#Connection <- dbConnect(Driver, dbname = "labuser", host = "localhost", port = 5432, user = "labuser")
+#DeepDiveData<-dbGetQuery(Connection,"SELECT docid, sentid, words FROM pbdb_fidelity.pbdb_fidelity_data")
+
+# RECORD INITIAL STATS
+# INITIAL NUMBER OF DOCUMENTS AND ROWS IN DEEPDIVEDATA: 
+StepOneDescription<-"Initial Data"
+# INITIAL NUMBER OF DOCUMENTS AND ROWS IN DEEPDIVEDATA:
+StepOneDocs<-length((unique(DeepDiveData[,"docid"])))
+StepOneRows<-nrow(DeepDiveData)
+StepOneUnits<-"NA"
+StepOneTuples<-"NA"
+
+# Search for the word "formation" in all DeepDiveData sentences
