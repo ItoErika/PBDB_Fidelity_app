7d7ed83b8fc4620ea98118026f2c4b539270f3b7
Erika Ito <etito@wisc.edu>
2017-03-13 12:35:54 -0500

    Update Stats.R

--- Analysis/Stats.R
+++ Analysis/Stats.R
@@ -1,1 +1,13 @@
+# Extract sedimentary units from the Macrostrat API
+UnitsURL<-"https://macrostrat.org/api/units?lith_class=sedimentary&project_id=1&response=long&format=csv"
+UnitURL<-RCurl::getURL(UnitsURL)
+UnitsFrame<-read.csv(text=UnitURL, header=TRUE)
 
+# Download data for geologic formations from the Macrostrat database API
+FormationsURL<-"https://macrostrat.org/api/defs/strat_names?rank=fm&format=csv"
+FormationsURL<-RCurl::getURL(FormationsURL)
+FormationsFrame<-read.csv(text=FormationsURL, header=TRUE)
+
+# Subset UnitsFrame to only include formations
+FormationData<-subset(UnitsFrame, UnitsFrame[,"strat_name_long"]%in%FormationsFrame[,"strat_name_long"])
+# Find the number of unique sedimentary units 
