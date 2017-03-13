4d25b24785f393c9eaff0fb212d9a9ad7d798a0c
Andrew Zaffos <aazaff@gmail.com>
2017-03-13 13:25:00 -0500

    Update Stats.R

--- Analysis/Stats.R
+++ Analysis/Stats.R
@@ -8,6 +8,12 @@ FormationsURL<-"https://macrostrat.org/api/defs/strat_names?rank=fm&format=csv"
 FormationsURL<-RCurl::getURL(FormationsURL)
 FormationsFrame<-read.csv(text=FormationsURL, header=TRUE)
 
+# Download Concept ids
+ConceptURL<-"https://macrostrat.org/api/defs/strat_names?all&format=csv"
+ConceptURL<-RCurl::getURL(ConceptURL)
+ConceptKey<-unique(read.csv(text=ConceptURL,header=TRUE)[,c("strat_name_id","concept_id")])
+
 # Subset UnitsFrame to only include formations
 FormationData<-subset(UnitsFrame, UnitsFrame[,"strat_name_long"]%in%FormationsFrame[,"strat_name_long"])
 # Find the number of unique sedimentary units 
+FormationData<-merge(FormationData,ConceptKey,by.x="strat_name_id",by.y="strat_name_id",all.x=TRUE)
