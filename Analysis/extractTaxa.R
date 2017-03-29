09a697f63385b18ae346172a73d8f74fdc7f392a
Erika Ito <etito@wisc.edu>
2017-03-29 15:56:27 -0500

    Add other approach

--- Analysis/extractTaxa.R
+++ Analysis/extractTaxa.R
@@ -58,3 +58,38 @@ Genera_Tuples[,"taxon_name"]<-gsub("%20", " ", Genera_Tuples[,"taxon_name"])
   
 # Assign row names
 rownames(Genera_Tuples)<-1:nrow(Genera_Tuples)
+    
+############################################## DIFFERENT APPROACH #####################################################
+
+# Download taxanomic names (genus and below) from the Paleobiology Database
+TaxaURL<-"https://paleobiodb.org/data1.2/taxa/list.csv?rank=max_genus&all_records"
+TaxaURL<-getURL(TaxaURL)
+Taxa<-read.csv(text=TaxaURL, header=TRUE)
+
+# Split Taxa into species and genera 
+Species<-Taxa[which(Taxa[,"taxon_rank"]=="species"),"taxon_name"]
+Genera<-Taxa[which(Taxa[,"taxon_rank"]=="genus"),"taxon_name"]
+
+# Load taxonomic name, docid tuples
+PBDB_Tuples<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/pbdb_docid_terms.csv", header=FALSE)
+# Assign column names
+colnames(PBDB_Tuples)<-c("docid", "taxon_name")
+
+# Subset PBDB_Tuples to only include taxonomic names that are at the genus or species level
+PBDB_Tuples<-subset(PBDB_Tuples, PBDB_Tuples[,"taxon_name"]%in%Genera|PBDB_Tuples[,"taxon_name"]%in%Species)
+    
+# Download final cleaned fidelity output data
+CleanedOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/CleanedOutput.csv")
+    
+# Subset CleanedOutput to only include documents from PBDB_Tuples
+Output<-subset(CleanedOutput, CleanedOutput[,"docid"]%in%PBDB_Tuples[,"docid"])    
+    
+dim(Output)
+# 1348
+    
+length(unique(Output[,"docid"]))
+# 724
+    
+length(unique(Output[,"Formation"]))
+# 353
+    
