1c92fed94c315a29037ce678b512ea48ddf07ed7
Erika Ito <etito@wisc.edu>
2017-05-08 15:16:03 -0500

    update for new run

--- Analysis/Stats.R
+++ Analysis/Stats.R
@@ -1,3 +1,4 @@
+# Load library
 library("RCurl")
 
 # Download outputs from app run
@@ -5,10 +6,11 @@ PaperStats<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/pbdb_fi
 AllDocuments<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/pbdb_fidelity_05May2017/AllDocuments.csv", row.names=1)
 Stats<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/pbdb_fidelity_05May2017/Stats.csv")
 InitialOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/pbdb_fidelity_05May2017/Fidelity_OutputData.csv", row.names=1)
-CleanedOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/CleanedOutput.csv", row.names=1)
-NoTraceOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoTraceOutput.csv", row.names=1)
-NoMicroOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoMicroOutput.csv", row.names=1)
-NoMicroNoTraceOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoMicroNoTraceOutput.csv", row.names=1)
+PBDBTupleOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/PBDBTupleOutput.csv")
+CleanedOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/CleanedOutput.csv")
+NoTraceOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoTraceOutput.csv")
+NoMicroOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoMicroOutput.csv")
+NoMicroNoTraceOutput<-read.csv("~/Documents/DeepDive/PBDB_Fidelity/Paper_Materials/NoMicroNoTraceOutput.csv")
 
 # Run date
 PaperStats[which(PaperStats[,"V1"]=="Fidelity run date"),"V2"]
@@ -63,57 +65,54 @@ length(unique(as.character(InitialOutput[which(InitialOutput[,"PBDB_occ"]==FALSE
 length(unique(as.character(InitialOutput[which(InitialOutput[,"PBDB_occ"]==TRUE),"Formation"])))
 # 1,119 non-candidate formations (in PBDB)
 length(unique(as.character(InitialOutput[which(InitialOutput[,"PBDB_occ"]==TRUE),"docid"])))
+# 2,974 documents in initial output mentioning non-candidate units
 
-# Number of candidate units, non-candidate units (in PBDB), and documents in cleaned output
+# Number of candidate units, non-candidate units (in PBDB), and documents in PBDBTupleOutput
+length(unique(as.character(PBDBTupleOutput[which(PBDBTupleOutput[,"PBDB_occ"]==FALSE),"Formation"])))
+# 702 candidate formations
+length(unique(as.character(PBDBTupleOutput[which(PBDBTupleOutput[,"PBDB_occ"]==FALSE),"docid"])))
+# 991 documents in initial output mentioning candidate units
+length(unique(as.character(PBDBTupleOutput[which(PBDBTupleOutput[,"PBDB_occ"]==TRUE),"Formation"])))
+# 1,114 non-candidate formations (in PBDB)
+length(unique(as.character(PBDBTupleOutput[which(PBDBTupleOutput[,"PBDB_occ"]==TRUE),"docid"])))
+# 2,913 documents mentioning non-candidate units
+
+# Number of candidate units, non-candidate units (in PBDB), and documents in CleanedOutput
 length(unique(as.character(CleanedOutput[which(CleanedOutput[,"PBDB_occ"]==FALSE),"Formation"])))
-# 610 candidate formations
+# 604 candidate formations
 length(unique(as.character(CleanedOutput[which(CleanedOutput[,"PBDB_occ"]==FALSE),"docid"])))
-# 853 documents in cleaned output mentioning candidate units
+# 828 documents in cleaned output mentioning candidate units
 length(unique(as.character(CleanedOutput[which(CleanedOutput[,"PBDB_occ"]==TRUE),"Formation"])))
-# 1,047 non-candidate formations (in PBDB)
+# 1,043 non-candidate formations (in PBDB)
 length(unique(as.character(CleanedOutput[which(CleanedOutput[,"PBDB_occ"]==TRUE),"docid"])))
-# 2,722 documents in initial output mentioning non-candidate units
+# 2,666 documents in initial output mentioning non-candidate units
 
 # Number of candidate units, non-candidate units (in PBDB), and documents in NoTraceOutput
 length(unique(as.character(NoTraceOutput[which(NoTraceOutput[,"PBDB_occ"]==FALSE),"Formation"])))
-# 594 candidate formations
+# 588 candidate formations
 length(unique(as.character(NoTraceOutput[which(NoTraceOutput[,"PBDB_occ"]==FALSE),"docid"])))
-# 807 documents in cleaned output mentioning candidate units
+# 782 documents in cleaned output mentioning candidate units
 length(unique(as.character(NoTraceOutput[which(NoTraceOutput[,"PBDB_occ"]==TRUE),"Formation"])))
-# 1,032 non-candidate formations (in PBDB)
+# 1,028 non-candidate formations (in PBDB)
 length(unique(as.character(NoTraceOutput[which(NoTraceOutput[,"PBDB_occ"]==TRUE),"docid"])))
-# 2,617 documents in cleaned output mentioning non-candidate units
+# 2,563 documents in cleaned output mentioning non-candidate units
 
 # Number of candidate units, non-candidate units (in PBDB), and documents in NoMicroOutput
 length(unique(as.character(NoMicroOutput[which(NoMicroOutput[,"PBDB_occ"]==FALSE),"Formation"])))
-# 606 candidate formations
+# 600 candidate formations
 length(unique(as.character(NoMicroOutput[which(NoMicroOutput[,"PBDB_occ"]==FALSE),"docid"])))
-# 851 documents in cleaned output mentioning candidate units
+# 826 documents in cleaned output mentioning candidate units
 length(unique(as.character(NoMicroOutput[which(NoMicroOutput[,"PBDB_occ"]==TRUE),"Formation"])))
-# 1,044 non-candidate formations (in PBDB)
+# 1,040 non-candidate formations (in PBDB)
 length(unique(as.character(NoMicroOutput[which(NoMicroOutput[,"PBDB_occ"]==TRUE),"docid"])))
-# 2,709 documents in cleaned output mentioning non-candidate units
+# 2,653 documents in cleaned output mentioning non-candidate units
 
 # Number of candidate units, non-candidate units (in PBDB), and documents in NoMicroNoTraceOutput
 length(unique(as.character(NoMicroNoTraceOutput[which(NoMicroNoTraceOutput[,"PBDB_occ"]==FALSE),"Formation"])))
-# 590 candidate formations
+# 584 candidate formations
 length(unique(as.character(NoMicroNoTraceOutput[which(NoMicroNoTraceOutput[,"PBDB_occ"]==FALSE),"docid"])))
-# 805 documents in cleaned output mentioning candidate units
+# 780 documents in cleaned output mentioning candidate units
 length(unique(as.character(NoMicroNoTraceOutput[which(NoMicroNoTraceOutput[,"PBDB_occ"]==TRUE),"Formation"])))
-# 1,029 non-candidate formations (in PBDB)
+# 1,025 non-candidate formations (in PBDB)
 length(unique(as.character(NoMicroNoTraceOutput[which(NoMicroNoTraceOutput[,"PBDB_occ"]==TRUE),"docid"])))
-# 2,604 documents in cleaned output mentioning non-candidate units
-
-
-
-
-
-length(unique(as.character(PBDB_Tuple_Output[which(PBDB_Tuple_Output[,"PBDB_occ"]==FALSE),"Formation"])))
-# 618 candidate formations
-length(unique(PBDB_Tuple_Output[which(PBDB_Tuple_Output[,"PBDB_occ"]==FALSE),"docid"]))
-# 935 documents in cleaned output mentioning candidate units
-length(unique(as.character(PBDB_Tuple_Output[which(PBDB_Tuple_Output[,"PBDB_occ"]==TRUE),"Formation"])))
-# 1,031 non-candidate formations (in PBDB)
-length(unique(PBDB_Tuple_Output[which(PBDB_Tuple_Output[,"PBDB_occ"]==TRUE),"docid"]))
-# 3,358 documents in cleaned output mentioning non-candidate units
-
+# 2,550 documents in cleaned output mentioning non-candidate units
