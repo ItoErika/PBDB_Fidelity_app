63c9603b1d1d0d32d6cdf3592fb21c9ecb97039a
Erika Ito <etito@wisc.edu>
2018-08-20 17:07:04 -0400

    update functions

--- Development/Fix_Fidelity.R
+++ Development/Fix_Fidelity.R
@@ -1,39 +1,73 @@
+##########################################################################################################################################
+############################################################# LOAD LIBRARIES #############################################################
+##########################################################################################################################################
+library(readr)
+
+##########################################################################################################################################
+############################################################### FUNCTIONS ###############################################################
+##########################################################################################################################################
+
 # Parse the NLP strings into a matrix format
 parseSentence<-function(Sentence,Parameters=c("words","dep_paths","dep_parents")) {
-  Sentence<-setNames(gsub("\"\"","SPACESUB",Sentence),names(Sentence))
-  Sentence<-setNames(gsub("\",\"","COMMASUB",Sentence),names(Sentence))
-  WordsMatrix<-sapply(Sentence[Parameters],function(x) strsplit(substring(x,2,nchar(x)-1),","))
-  WordsMatrix<-do.call(rbind,WordsMatrix)
-  WordsMatrix[which(WordsMatrix=="COMMASUB")]<-","
-  WordsMatrix[which(WordsMatrix=="SPACESUB")]<-""
-  colnames(WordsMatrix)<-1:ncol(WordsMatrix)
-  return(WordsMatrix)
-}
+        Sentence<-setNames(cleanPunctuation(Sentence),names(Sentence))
+        if ("words"%in%names(Sentence)) {Sentence["words"]<-trueCommas(Sentence["words"])}
+        WordsMatrix<-sapply(Sentence[Parameters],function(x) strsplit(x,","))
+        WordsMatrix<-do.call(rbind,WordsMatrix)
+        WordsMatrix[which(WordsMatrix=="COMMASUB")]<-","
+        WordsMatrix[which(WordsMatrix=="SPACESUB")]<-""
+        colnames(WordsMatrix)<-1:ncol(WordsMatrix)
+        return(WordsMatrix)
+        }
 
-# Sees if numbers are consecutive
-findConsecutive<-function(NumericSequence) {
-  Breaks<-c(0,which(diff(NumericSequence)!=1),length(NumericSequence))
-  ConsecutiveList<-lapply(seq(length(Breaks)-1),function(x) NumericSequence[(Breaks[x]+1):Breaks[x+1]])
-  return(ConsecutiveList)
-}
+# Account for actual phrases or numbersets with commas - e.g., "19,560,238" -> 19560238
+trueCommas<-function(Words) {
+        InsideQuotes<-regmatches(Words, gregexpr('"[^"]*"',Words))[[1]]
+        if (length(InsideQuotes)<1) {return(Words)}
+        Replacements<-gsub(",","",InsideQuotes)
+        for (i in 1:length(InsideQuotes)) {
+                Words<-noquote(gsub(InsideQuotes[i],Replacements[i],Words))
+                }
+        return(Words)
+        }
+
+# Remove or replace problematic punctuation
+# Even though this is redundnat with trueCommas it applies to more fields
+cleanPunctuation<-function(Sentence) {
+        Sentence<-gsub("\"\"","SPACESUB",Sentence)
+        Sentence<-gsub("\",\"","COMMASUB",Sentence) 
+        Sentence<-gsub("\\{|\\}","",Sentence)
+        Sentence<-gsub("-LRB-","(",Sentence)
+        Sentence<-gsub("-RRB-",")",Sentence)
+        Sentence<-gsub("-LCB-","{",Sentence)
+        Sentence<-gsub("-RCB-","}",Sentence)
+        return(Sentence)
+        }
 
 # A function to find proper noun clusters
 findCluster<-function(Sentence,Parameters=c("words","poses")) {
-  ParsedSentence<-parseSentence(Sentence,Parameters)
-  FindConsecutive<-findConsecutive(which(ParsedSentence["poses",]=="NNP"))
-  Proper<-sapply(FindConsecutive,function(x) paste(unname(ParsedSentence["words",x]),collapse=" "))
-  Proper<-unname(cbind(Sentence["docid"],Sentence["sentid"],Proper))
-  return(Proper)
-}
+        ParsedSentence<-parseSentence(Sentence,Parameters)
+        FindConsecutive<-findConsecutive(which(ParsedSentence["poses",]=="NNP"))
+        Proper<-sapply(FindConsecutive,function(x) paste(unname(ParsedSentence["words",x]),collapse=" "))
+        Proper<-unname(cbind(Sentence["docid"],Sentence["sentid"],Proper))
+        return(Proper)
+        }
+
+# Sees if numbers are consecutive
+findConsecutive<-function(NumericSequence) {
+        Breaks<-c(0,which(diff(NumericSequence)!=1),length(NumericSequence))
+        ConsecutiveList<-lapply(seq(length(Breaks)-1),function(x) NumericSequence[(Breaks[x]+1):Breaks[x+1]])
+        return(ConsecutiveList)
+        }
 
 
 docs<-as.data.frame(read_delim('C:/Users/erikai94/Documents/UW_Madison/GDD/test_docs/sentences_nlp352', delim='\t', col_names = FALSE))
-# Remove { and } from docs
-docs<-gsub("\\{|\\}", "", docs)
 colnames(docs)<-c('docid','sentid','wordidx','words','poses','ners','lemmas','dep_paths','dep_parents')
 
 
 
+                 
+                 
+                 
 
 clean_words <- function(x) {
   
@@ -61,6 +95,3 @@ for (i in 1:nrow(docs)){
 parseSentence(docs[i,])
   print(i)
 }
-
-# Remove bracket symbols ({ and }) from SubsetDeepDive sentences
-SubsetDeepDive[,"words"]<-gsub("\\{|\\}", "", SubsetDeepDive[,"words"])
