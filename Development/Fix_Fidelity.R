cb2cc8febea8eeb583d07059e2a7469633ebab1d
Erika Ito <etito@wisc.edu>
2018-08-21 10:42:40 -0400

    add if

--- Development/Fix_Fidelity.R
+++ Development/Fix_Fidelity.R
@@ -17,7 +17,7 @@ parseSentence<-function(Sentence,Parameters=c("words","dep_paths","dep_parents")
         WordsMatrix[which(WordsMatrix=="SPACESUB")]<-""
         colnames(WordsMatrix)<-1:ncol(WordsMatrix)
         return(WordsMatrix)
-        }
+        }                 
 
 # Account for actual phrases or numbersets with commas - e.g., "19,560,238" -> 19560238
 trueCommas<-function(Words) {
@@ -47,9 +47,11 @@ cleanPunctuation<-function(Sentence) {
 # A function to find proper noun clusters
 findCluster<-function(Sentence,Parameters=c("words","poses")) {
         ParsedSentence<-parseSentence(Sentence,Parameters)
+        if(all(is.na(ParsedSentence))) {return(setNames(rep(NA,3), c("docid", "sentid", "Proper")))}
         FindConsecutive<-findConsecutive(which(ParsedSentence["poses",]=="NNP"))
         Proper<-sapply(FindConsecutive,function(x) paste(unname(ParsedSentence["words",x]),collapse=" "))
         Proper<-unname(cbind(Sentence["docid"],Sentence["sentid"],Proper))
+        colnames(Proper)<-c("docid","sentid","Proper") 
         return(Proper)
         }
 
@@ -58,6 +60,20 @@ findConsecutive<-function(NumericSequence) {
         Breaks<-c(0,which(diff(NumericSequence)!=1),length(NumericSequence))
         ConsecutiveList<-lapply(seq(length(Breaks)-1),function(x) NumericSequence[(Breaks[x]+1):Breaks[x+1]])
         return(ConsecutiveList)
+        }
+
+adjacencyPath<-function(Sentence,Path="amod") {
+        ParsedSentence<-parseSentence(Sentence,c("words","dep_paths","dep_parents"))
+        if (all(is.na(ParsedSentence))) {return(setNames(rep(NA,4),c("docid","sentid","child","parent")))}
+        PathMods<-as.matrix(ParsedSentence[,which(ParsedSentence["dep_paths",]==Path)])
+        if (length(PathMods)<1) {return(setNames(rep(NA,4),c("docid","sentid","child","parent")))}
+        FinalList<-vector("list")
+        for (j in seq_len(ncol(PathMods))) {
+                FinalList[[length(FinalList)+1]]<-c(Sentence[1:2],PathMods["words",j],ParsedSentence["words",as.numeric(PathMods["dep_parents",j])])
+                }
+        FinalTable<-do.call(rbind,FinalList)
+        colnames(FinalTable)<-c("docid","sentid","child","parent")
+        return(FinalTable)
         }
 
 ##########################################################################################################################################
@@ -77,18 +93,30 @@ docs<-as.data.frame(read_delim('sentences_nlp352', delim='\t', col_names = FALSE
 colnames(docs)<-c('docid','sentid','wordidx','words','poses','ners','lemmas','dep_paths','dep_parents')
 
 # Subset the test document set (first 2 documents)
-docs<-docs[1:max(which(docs[,"docid"]==unique(docs[,"docid"])[2])),]
+docs<-docs[1:max(which(docs[,"docid"]==unique(docs[,"docid"])[50])),]
 
 # Clean the punctuation in all of the documents
-test<-t(apply(docs[1:4113,],1 , cleanPunctuation))
+docs<-t(apply(docs[1:4113,],1 , cleanPunctuation))
                                 
-parseSentence(test[1,])                                
+parseSentence(docs[1,])                                
+
 
                  
                  
-                 
 
-clean_words <- function(x) {
+
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+  clean_words <- function(x) {
   
   . <- NULL
   
@@ -115,25 +143,8 @@ parseSentence(test[i,])
   print(i)
 }
                                 
-ParsedSentence<-vector("list", length=nrow(test))                           
-for (i in 1:nrow(test)){
-        tryCatch(ParsedSentence[[i]]<-parseSentence(test[i,]), warning=function(i) {print(i)})
-        }
-        
-        
-        
-        
-        parseSentence<-function(Sentence,Parameters=c("words","dep_paths","dep_parents")) {
-        Sentence<-setNames(cleanPunctuation(Sentence),names(Sentence))
-        if ("words"%in%names(Sentence)) {Sentence["words"]<-trueCommas(Sentence["words"])}
-        WordsMatrix<-sapply(Sentence[Parameters],function(x) strsplit(x,","))
-  if (sum(diff(sapply(WordsMatrix,length)))!=0) {return(NA)}
-        WordsMatrix<-do.call(rbind,WordsMatrix)
-        WordsMatrix[which(WordsMatrix=="COMMASUB")]<-","
-        WordsMatrix[which(WordsMatrix=="SPACESUB")]<-""
-        colnames(WordsMatrix)<-1:ncol(WordsMatrix)
-        return(WordsMatrix)
-        }
+       
+
         
         
         
